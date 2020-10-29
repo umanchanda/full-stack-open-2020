0036d4e9a2fccf5cef5a0a6a27f56db9400948f4
Uday Manchanda <uday.manchanda14@gmail.com>
2020-10-29 04:41:32 -0400

    fixed bugs and completed exercise 2.4-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,33 +1,33 @@
 import React from 'react'
 
-const Header = ({ courses }) => {
-    return courses.map(course => <h1 key={course.id}>{course.name}</h1>)
+const Header = ({ course }) => {
+    return <h2 key={course.id}>{course.name}</h2>
 }
 
-const Total = ({ courses }) => {
-    const parts = courses.map(course => course.parts)
-    // const sum = parts.reduce((accumulator, currentValue) => accumulator + currentValue)
-    const sum = parts[0][0].exercises + parts[0][1].exercises + parts[0][2].exercises + parts[0][3].exercises
-    const sum2 = parts[1][0].exercises + parts[1][1].exercises
-    return <p>total of {sum} exercises {sum2}</p>
+const Total = ({ course }) => {
+    const sum = course.parts.map(part => part.exercises)
+                            .reduce((accumulator, currentValue) => accumulator + currentValue)
+    return <p>total of {sum} exercises</p>
 }
 
 const Part = (props) => {
     return props.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
 }
 
-const Content = ({ courses }) => {
-    return courses.map(course => <Part key={course.id} parts={course.parts} />)
+const Content = ({ course }) => {
+    return <Part key={course.id} parts={course.parts} />
 }
 
 const Course = ({ courses }) => {
-    return (
-        <div>
-            <Header courses={courses} />
-            <Content courses={courses} />
-            <Total courses={courses} />
-        </div>
-    )
+    return courses.map(course => {
+        return (
+            <div>
+                <Header course={course} />
+                <Content course={course} />
+                <Total course={course} />
+            </div>
+        )
+    })
 }
 
 export default Course
