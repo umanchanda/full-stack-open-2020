44df1f816cd43531731b6b21308d1d44b9b611a5
Uday Manchanda <uday.manchanda14@gmail.com>
2020-11-07 23:30:08 -0500

    finished exercise 2.11 and fixed bugs

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import Person from './components/Person'
 import Filter from './components/Filter'
 
-const App = (props) => {
+const App = () => {
+
+  const [ persons, setPersons ] = useState([])
+  const [ newName, setNewName ] = useState('')  
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    const isPresent = persons.some(person => person.name.includes(newName));
+    
+    if (isPresent) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(prevPersons => prevPersons.concat(personObject));
+    }
+    
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+    setShowAll(!showAll)
+  }
+
+  const namesToShow = persons.map(person => person.name).filter(name => name.toLowerCase().includes(filter.toLowerCase()))
+  
   return (
     <div>
       <h1>Phonebook</h1>
-      <Filter persons={props.persons} />
-      <Person persons={props.persons} />
+
+      <div>filter show with <input value={filter} onChange={handleFilterChange}/></div>
+
+      <h2>add a  new</h2>
+      <form onSubmit={addNewPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>{persons.map(person => <Person key={person.id} name={person.name} number={person.number} />)}</ul>
+
+      <h2>Filtered</h2>
+      <ul>{namesToShow.map(name => <Filter key={name} name={name} />)}</ul>
     </div>
   )
 }
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -2,11 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js'
 
-const persons = [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-532532', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-523221', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6426326', id: 4 }
-]
-
-ReactDOM.render(<App persons={persons} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
