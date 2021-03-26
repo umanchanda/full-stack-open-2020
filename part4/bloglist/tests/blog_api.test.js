b15d27323b1051293374f0cfbdd6ab41fa22d656
Uday Manchanda <uday.manchanda14@gmail.com>
2021-03-26 06:22:07 -0400

    getting started on exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogPosts = [
+  {
+    title: 'My First Post',
+    author: 'Uday Manchanda',
+    url: 'https://umanchanda.github.io/blogs/My-First-Post',
+    likes: 7
+  }, 
+  {
+    title: 'My Second Post',
+    author: 'Uday Manchanda',
+    url: 'https://umanchanda.github.io/blogs/My-Second-Post',
+    likes: 3
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogPosts[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogPosts[1])
+  await blogObject.save()
+})
+
+test('verify correct number of blogs', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type',/application\/json/)
+    
+  expect(response.body).toHaveLength(initialBlogPosts.length + 1)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
